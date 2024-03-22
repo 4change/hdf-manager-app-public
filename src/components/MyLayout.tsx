0c397abfde8491633e22a6fa0b751b8c1aeea009
gongxf <gongxf@yunphant.com>
2024-03-22 16:46:29 +0800

    撑开整个页面

---
+++ src/components/MyLayout.tsx
@@ -0,0 +1,67 @@
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+
+const { Header, Sider, Content } = Layout;
+
+const MyLayout = ({children}: any) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    return (
+        <Layout
+            style={{ width: "100vw", height: "100vh" }  /** style 属性用于撑开整个页面，可去掉该属性再对比查看页面 */}
+        >
+            <Sider trigger={null} collapsible collapsed={collapsed}>
+                <div className="logo" />
+                <Menu
+                    theme="light"       /** 设置左侧菜单栏主题为 light，也可设置为 dark */
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: 'nav 1',
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: 'nav 2',
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header className="site-layout-background" style={{ padding: 0 }}>
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </Header>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                    }}
+                >
+                    {children}
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default MyLayout;
