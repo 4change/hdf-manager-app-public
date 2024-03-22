5625a70dfe56aac4eb553ed61a4c4a0fd8193be6
gongxf <gongxf@yunphant.com>
2024-03-22 15:08:19 +0800

    1-1. antd 国际化配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import 'antd/dist/antd.compact.css'   // 紧凑主题
+import zhCN from 'antd/es/locale/zh_CN';    // 国际化
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <ConfigProvider locale={zhCN}>  { /** 国际化 */}
+      <App />
+    </ConfigProvider>,
+  </Router>
 )
